cedcc3e89f4b37d2d0a8d3493da96294e9e52d92
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-26 14:15:17 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .querySelector("input[type=submit]")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      addItemToList();
+    });
+
+  function addItemToList() {
+    input = document.getElementById("new-task-description").value;
+    document.getElementById(
+      "tasks"
+    ).innerHTML += `<li>${input} <button id="button-for-${input}">X</button></li>`;
+
+    addDeleteButton(input);
+  }
+
+  function addDeleteButton(input) {
+    var button = document.getElementById(`button-for-${input}`);
+    button.addEventListener("click", () => {
+      var parent = button.parentNode;
+      parent.parentNode.removeChild(parent);
+    });
+  }
 });
